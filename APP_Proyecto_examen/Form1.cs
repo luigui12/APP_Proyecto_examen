e2db88a41d197366f957ae8f99c6b373d45c711b
luigui12 <luigui.castillo@pucese.edu.ec>
2019-12-04 11:27:16 -0500

    Agregar archivos de proyecto.

---
+++ APP_Proyecto_examen/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace APP_Proyecto_examen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.limite.Text = "";
+            this.suma.Text = "";
+        }
+    }
+}
